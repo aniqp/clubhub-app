a1b3566c9f033bc14e9b77a47dbcc3ea37f87d4d
Fraser Morrison <frazemor@hotmail.com>
2023-02-25 19:15:16 -0500

    Added navigation e2e test

---
+++ node-react-app/client/cypress/e2e/navigation.cy.js
@@ -0,0 +1,26 @@
+describe('Navigation Test', () => {
+    it('Can view the home page', () => {
+      cy.visit('/');
+      cy.contains('CLUBHUB');
+      cy.contains('University of Waterloo');
+    });
+
+    it('Can navigate to Explore page from navbar', () => {
+        cy.get('[href*="explore"]').click();
+        cy.contains('CLUBHUB');
+        cy.url().should('include', '/explore');
+    })
+
+    it('Can navigate to club details page from explore', () => {
+        cy.contains('Club Details').click({force: true});
+        cy.url().should('include', '/clubs/');
+        cy.contains('CLUBHUB');
+        cy.contains('Description');
+    })
+
+    it('Can navigate to Home from navbar', () => {
+        cy.get('[href="/"]').click();
+        cy.contains('CLUBHUB');
+        cy.contains('University of Waterloo');
+    })
+  });
