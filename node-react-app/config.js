3e77220f5eef1f186ae5410b76001696ba8951c1
Fraser Morrison <frazemor@hotmail.com>
2023-04-25 02:42:49 -0400

    Use local database

--- node-react-app/config.js
+++ node-react-app/config.js
@@ -1,8 +1,9 @@
 let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : process.env.MYSQL_USER || 'a8premji',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  database: 'f4morris'
+  host    : process.env.DB_HOST,
+  user    : process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 };
  
 module.exports = config;
